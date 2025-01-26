47b3b5417d4da0bf977ea929e7b3fb36b6475903
Aram <aramaramyants@gmail.com>
2025-01-26 23:51:20 +0400

    portals

--- src/App.js
+++ src/App.js
@@ -2,52 +2,13 @@ import './App.css';
 import RefsDemo from './components/RefsDemo.js';
 import FocusInput from './components/FocusInput.js';
 import FRParentInput from './components/FRParentInput.js';
+import PortalDemo from './components/PortalDemo.js';
 
 function App() {
   return (
       <div className="App">
-        <FRParentInput/>
-        {/* <FocusInput></FocusInput> */}
-        {/* <RefsDemo/> */}
-        {/*
-        <ParentComp/>
-        <Table></Table>
-        <FragmentDemo></FragmentDemo>
-        <LifecycleA></LifecycleA>
-        <Form></Form>
-
-        <h1 className='error'>Error</h1>
-        <h1 className={styles.success}>Success</h1>
-
-        <Inline></Inline>
-        <Stylesheet primary={true}></Stylesheet>
-        <NameList></NameList>
-        <UserGreeting />
-
-        <ParentComponent></ParentComponent>
-        <EventBind></EventBind>0
-        <FunctionClick></FunctionClick>
-        <ClassClick></ClassClick>
-
-        <Counter />
-        <Message />
-
-        <Greet name="Aram">
-          <p>This is children props</p>
-        </Greet>
-        <Greet name="Roba"/>
-        <Greet name="Miqo"/>
-
-        <Welcome name="Aram">
-          <button type="button">Lyavs TU?</button>
-        </Welcome>
-        <Welcome name="Roba" />
-        <Welcome name="Miqo" />
-        <Hello />
-
-        <Greet name="Aram" heroName="Barmen" />
-        */}
-        </div>
+        <PortalDemo></PortalDemo>
+      </div>
   )
 }
 
