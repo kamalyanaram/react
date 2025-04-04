ae4c1f9b8ea07a2a2f226b2d7d5082f46d81187b
Aram <aramaramyants@gmail.com>
2025-04-04 20:59:48 +0400

    Render Props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
 import './App.css'
-import ClickCounter from './components/ClickCounter'
-import HoverCounter from './components/HoverCounter'
+import ClickCounterTwo from './components/ClickCounterTwo'
+import HoverCounterTwo from './components/HoverCounterTwo'
+import User from './components/User'
+import Counter from './components/Counter'
 
 export class App extends Component {
   render() {
     return (
       <div className='App'>
-        <ClickCounter name='Aram'/>
-        <HoverCounter/>
+        <Counter
+          render = { (count, incrementCount) => (
+            <ClickCounterTwo count={count} incrementCount={incrementCount} />
+          )}
+        />
+        <Counter
+          render = { (count, incrementCount) => (
+            <HoverCounterTwo name="Nice" count={count} incrementCount={incrementCount} />
+          )}
+        />
+        {/* <ClickCounterTwo/>
+        <HoverCounterTwo/>
+        <User render={ (isLoggedIn) => isLoggedIn ? 'Aramyants' : 'Guest' }/> */}
       </div>
     )
   }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,48 +1,24 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         count: 0
-      }
-    }
-    
-    increment() {
-        // this.setState(
-        //     {
-        //     count: this.state.count + 1
-        //     },
-        //     () => {
-        //         console.log('Callback value', this.state.count)
-        //     }
-        // )
+  constructor(props) {
+    super(props)
 
-        this.setState((prevState, props) => ({
-            count: prevState.count + 1
-        }),
-            () => {
-                console.log('Callback value', this.state.count)
-            }
-        )
-        
-        console.log(this.state.count)
+    this.state = {
+       count: 0
     }
+  }
 
-    incrementFive() {
-        this.increment()
-        this.increment()
-        this.increment()
-        this.increment()
-        this.increment()
-    }
-    
+  incrementCount = () => {
+    this.setState(prevState => {
+      return { count: prevState.count + 1}
+    })
+  }
+
   render() {
     return (
       <div>
-       <div> count - {this.state.count}</div>
-       <button onClick={() => this.incrementFive()}>Increment</button>
+       {this.props.render(this.state.count, this.incrementCount)}
       </div>
     )
   }
