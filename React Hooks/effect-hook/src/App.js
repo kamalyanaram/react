89f5eceed92b37ed93e0c1568bd6e9dad4cebcf3
kamalyanaram <aramaramyants@gmail.com>
2025-06-04 14:40:45 +0400

    Progress on React hooks: finished useState, started useEffect

---
+++ React Hooks/effect-hook/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ClassMouse from './components/ClassMouse';
+import HookMouse from './components/HookMouse';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassMouse/> */}
+      <HookMouse />
+    </div>
+  );
+}
+
+export default App;
