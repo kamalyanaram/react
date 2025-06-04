18fd494514aa7ac0bc7f853626f3b62608290f08
kamalyanaram <aramaramyants@gmail.com>
2025-06-04 14:41:20 +0400

    Progress on React hooks: finished useState, started useEffect

--- React Hooks/state-hook/src/App.js
+++ React Hooks/state-hook/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
-import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterFour from './components/HookCounterFour';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter/>
-      {/* <ClassCounter /> */}
-      <HookCounterTwo></HookCounterTwo>
+      <HookCounterFour></HookCounterFour>
     </div>
   );
 }
--- React Hooks/state-hook/src/components/HookCounterThree.js
+++ React Hooks/state-hook/src/components/HookCounterThree.js
@@ -1,21 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
 function HookCounterThree() {
-
-  const [name, setName] = useState({firstName: '', lastName: ''})
+  const [name, setName] = useState({ firstName: '', lastName: '' });
 
   return (
     <div>
       <form action="">
-        <input type="text" name="" id="" value={name.firstName} onChange={{e => setName}}/>
-        <input type="text" name="" id="" />
+        <input
+          type="text"
+          name=""
+          id=""
+          value={name.firstName}
+          onChange={(e) => setName({ ...name, firstName: e.target.value })}
+        />
+        <input
+          type="text"
+          name=""
+          id=""
+          value={name.lastName}
+          onChange={(e) => setName({ ...name, lastName: e.target.value })}
+        />
         <h2>Your first name is - {name.firstName}</h2>
         <h2>Your last name is - {name.lastName}</h2>
-
+        <h2>{JSON.stringify(name)}</h2>
       </form>
     </div>
-  )
+  );
 }
 
-export default HookCounterThree
-
+export default HookCounterThree;
